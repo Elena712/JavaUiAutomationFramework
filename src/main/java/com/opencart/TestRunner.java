7c5f4ff9fec40972a3fcc726a006c901632d1202
GoldenLady <elenabuhaniuc@gmail.com>
2023-10-17 11:52:32 +0300

    Implement the driver manager

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,12 +1,32 @@
 package com.opencart;
 
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class TestRunner {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        String currentWindowName = driver.getWindowHandle();
+        //new Window Code
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.google.com/");
+        Thread.sleep(2);
+        System.out.println(driver.getTitle());
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+        driver.get("https://tekwill.md/");
+        Thread.sleep(1);
+        System.out.println(driver.getTitle());
+        driver.quit();
+
+        System.out.println("The execution is over");
+
 
     }
 }
--- src/main/java/com/opencart/managers/ExampleManagers.java
+++
@@ -1,4 +0,0 @@
-package com.opencart.managers;
-
-public class ExampleManagers {
-}
